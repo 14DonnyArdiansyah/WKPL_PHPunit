6a624051b2799548be95df76c6851c13e815b5ea
14DonnyArdiansyah <donny14@polije.ac.id>
2023-10-20 08:17:03 +0700

    first commit

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,13 +1,17 @@
 <?php
+//import PHPUnit
 use PHPUnit\Framework\TestCase;
 require_once "WordCount.php";
 class SimpleTest extends TestCase
 {
     public function testCountWords()
     {
+        //membuat objek class
         $Wc = new WordCount();
         $TestSentence = "My name is Donny"; // 4 Kata ..
         $WordCount = $Wc->countWords($TestSentence);
+
+        //melakukan pengujian apakah hasil yang dikembalikan oleh `countWords` sama dengan 4
         $this->assertEquals(4, $WordCount);
     }
 }
--- WordCount.php
+++ WordCount.php
@@ -1,8 +1,11 @@
 <?php
+//metode untuk menghitung jumlah kata dalam sebuah kalimat
 class WordCount
 {
     public function countWords($sentence)
     {
+        //explode berfungsi untuk memecah kalimat
+        //count berfungsi menghitung jumlah elemen dalam array 
         return count (explode(" ",$sentence));
     }
 }
